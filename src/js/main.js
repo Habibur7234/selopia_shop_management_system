e1c57c9698fd4693b319cfa3428ed9ee741e5acb
habiburrahman <habibur72345@gmail.com>
2022-10-10 18:29:55 +0600

    initial 3

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ let rowIndex, rowData;
 const notyf = new Notyf();
 
 //init shop datatable and load data
-let t1= $('#shopTable').DataTable( {
+let shop_table= $('#shop_dataTable').DataTable( {
     order: [[0, 'desc']],
     "columnDefs": [
         { 'visible': false, 'targets': 0 },
@@ -27,9 +27,70 @@ let t1= $('#shopTable').DataTable( {
         searchPlaceholder: "Search..."
     },
     buttons: [
-        'excel', 'pdf', 'print',
-        '<button id="edit_button_id"  class="btn btn-light btn-outline-gray-700 shadow-none" type="button" data-bs-toggle="modal" data-bs-target="#Submit_shop_modal" ><svg class="icon icon-xs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg></button>'
-    ],
+        {
+            extend: 'print',
+            title: 'Shop Information',
+            orientation: 'landscape',
+            exportOptions: {
+                columns: [  1, 2, 3 ],
+                modifier: {
+                    page: 'current'
+                }
+            },
+            pageSize: 'LEGAL',
+            customize: function ( win ) {
+                $(win.document.body)
+                    .css( 'font-size', '15pt' )
+                $(win.document.body).find( 'th' )
+                    .css( {
+                        "font-size": 'inherit',
+                        "text-align":'center',
+                    })
+                    .addClass( 'compact' )
+                $(win.document.body).find( 'table' )
+                    .css( 'font-size', 'inherit' )
+                    .css('text-align','center')
+
+            }
+        },
+    {
+        extend: 'excelHtml5',
+        title: 'Shop Information',
+        exportOptions: {
+            columns: [  1, 2, 3 ]
+
+        },
+
+    },
+    {
+        extend: 'pdf',
+        exportOptions: {
+            columns: [ 1, 2, 3 ],
+            modifier: {
+                page: 'current'
+            }
+        },
+        pageSize: 'LEGAL',
+        title: 'Shop Information',
+        customize: function (doc) {
+            doc.content[1].table.widths = [
+                '20%',
+                '35%',
+                '45%',
+            ]
+            let rowCount = doc.content[1].table.body.length;
+            for (let i = 1; i < rowCount; i++) {
+                doc.content[1].table.body[i][0].alignment = 'center';
+                doc.content[1].table.body[i][1].alignment = 'center';
+                doc.content[1].table.body[i][2].alignment = 'center';
+            }
+        }
+    },
+
+    '<button id="shop_addBtn"  toggle="tooltip" title="Add New" class="btn btn-light btn-outline-gray-700 shadow-none" type="button" data-bs-toggle="modal" data-bs-target="#add_shop_modal" ><svg class="icon icon-xs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg></button>'
+
+],
+
     columns: [
         { data: 'id' },
         { data: 'name' },
@@ -37,8 +98,8 @@ let t1= $('#shopTable').DataTable( {
         { data: 'address' },
         {data: 'id',
             render: function(){
-                return '<button id="edit_button_id"  class="btn btn-primary" toggle="tooltip" title="Edit" type="button" data-bs-toggle="modal"   data-bs-target="#update_shop_modal1" ><svg class="icon icon-xs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg></button>  '
-                    +'<button   id="delete_button_id"  class="btn btn-danger" toggle="tooltip" title="Delete" data-bs-toggle="modal"   data-bs-target="#delete_shop_modal"><svg class="icon icon-xs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg></button>'
+                return '<button id="update_shopBtn"  class="btn btn-primary" toggle="tooltip" title="Edit" type="button" data-bs-toggle="modal"   data-bs-target="#update_shop_modal" ><svg class="icon icon-xs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg></button>  '
+                    +'<button   id="delete_shopBtn"  class="btn btn-danger" toggle="tooltip" title="Delete" data-bs-toggle="modal"   data-bs-target="#delete_shop_modal"><svg class="icon icon-xs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg></button>'
             }
         },
     ]
@@ -49,12 +110,10 @@ $(document).ready(function () {
 
     /* ### Update Data Start ### */
     // EDIT button
-    $('#shopTable tbody').on('click', '#edit_button_id', function () {
+    $('#shop_dataTable tbody').on('click', '#update_shopBtn', function () {
         // getting parent row index and data
-        rowIndex = t1.row($(this).parents('tr')).index();
-        rowData =  t1.row($(this).parents('tr')).data();
-
-        console.log(rowData.id);
+        rowIndex = shop_table.row($(this).parents('tr')).index();
+        rowData =  shop_table.row($(this).parents('tr')).data();
 
         // setting row values to update modal input boxes
         $("#editName").val(rowData.name);
@@ -63,7 +122,7 @@ $(document).ready(function () {
     })
 
     // Update Button
-    $("#update_modal_id").click(function(){
+    $("#update_shop").click(function(){
 
 
         if( $('#update_shop_form')[0].checkValidity() ) {
@@ -84,23 +143,23 @@ $(document).ready(function () {
                 success: function()
                 {
                     // hide modal
-                    const modal = bootstrap.Modal.getInstance($("#update_shop_modal1"));
+                    const modal = bootstrap.Modal.getInstance($("#update_shop_modal"));
                     modal.hide();
 
                     //Set default button text again
-                    $("#update_modal_id").text('Update Info');
+                    $("#update_shop").text('Update Info');
 
-                    let currentPage = t1.page();
+                    let currentPage = shop_table.page();
 
                     // update datatable
-                    t1.row(rowIndex).data( rowData ).draw();
+                    shop_table.row(rowIndex).data( rowData ).draw();
 
                     // redrawing to original page
-                    t1.page(currentPage).draw( 'page' );
+                    shop_table.page(currentPage).draw( 'page' );
 
                     // highlighting newly added row
-                    $( t1.row(rowIndex).nodes() ).addClass( 'selected' );
-                    setTimeout(function () { $( t1.row(rowIndex).nodes() ).removeClass( 'selected' ); }, 2000);
+                    $( shop_table.row(rowIndex).nodes() ).addClass( 'selected' );
+                    setTimeout(function () { $( shop_table.row(rowIndex).nodes() ).removeClass( 'selected' ); }, 2000);
 
                     // notification
                     notyf.success({
@@ -109,14 +168,11 @@ $(document).ready(function () {
                         icon: false
                     });
 
-                    // reset global variable value
-                    // rowIndex = undefined;
-                    // rowData = undefined;
                 },
                 error: function()
                 {
                     //Set default button text again
-                    $("#update_modal_id").text('Update Info');
+                    $("#update_shop").text('Update Info');
 
                     //Notification
                     notyf.error({
@@ -136,7 +192,7 @@ $(document).ready(function () {
     /* ### Update Data End ### */
 
     /* ### Add Data Start ### */
-    $("#post_modal_id").click(function(){
+    $("#add_shop").click(function(){
 
         if( $('#shop_form')[0].checkValidity() ) {
             $(this).text('Submitting..');
@@ -154,10 +210,10 @@ $(document).ready(function () {
                 success: function(data)
                 {
                     //add row from input fields
-                    let newRowIndex = t1.row.add(data).draw();
+                    let newRowIndex = shop_table.row.add(data).draw();
 
-                    $("#post_modal_id").text('Submit');
-                    const modal = bootstrap.Modal.getInstance($("#Submit_shop_modal"));
+                    $("#add_shop").text('Submit');
+                    const modal = bootstrap.Modal.getInstance($("#add_shop_modal"));
                     modal.hide();
 
                     //reset input Field
@@ -165,14 +221,14 @@ $(document).ready(function () {
                     $('.input').val('');
 
                     // reset search
-                    t1.search('');
+                    shop_table.search('');
 
                     // re-ordering to default
-                    t1.order( [ 0, 'desc' ] ).draw();
+                    shop_table.order( [ 0, 'desc' ] ).draw();
 
                     // highlighting newly added row
-                    $( t1.row(newRowIndex.index()).nodes() ).addClass( 'selected' );
-                    setTimeout(function () { $( t1.row(newRowIndex.index()).nodes() ).removeClass( 'selected' ); }, 2000);
+                    $( shop_table.row(newRowIndex.index()).nodes() ).addClass( 'selected' );
+                    setTimeout(function () { $( shop_table.row(newRowIndex.index()).nodes() ).removeClass( 'selected' ); }, 2000);
 
                     //Success Notification
                     notyf.success({
@@ -184,7 +240,7 @@ $(document).ready(function () {
                 error: function()
                 {
                     //Set default button text again
-                    $("#post_modal_id").text('Submit');
+                    $("#add_shop").text('Submit');
 
                     //Notification
                     notyf.error({
@@ -205,15 +261,15 @@ $(document).ready(function () {
 
     /* ### Delete Data Start ### */
     // DELETE button
-    $('#shopTable tbody').on('click', '#delete_button_id', function () {
-        rowData =  t1.row($(this).parents('tr')).data();
-        rowIndex = t1.row($(this).parents('tr')).index();
+    $('#shop_dataTable tbody').on('click', '#delete_shopBtn', function () {
+        rowData =  shop_table.row($(this).parents('tr')).data();
+        rowIndex = shop_table.row($(this).parents('tr')).index();
+        // Set value on Modal
+        $("#sName").text(`Are you sure you want to delete "${rowData.name}"?`);
     });
 
     // DELETE Confirmation button
-    $("#delete_modal_id").click(function() {
-
-        console.log('deleting something');
+    $("#delete_shop").click(function() {
 
         $(this).text('Deleting...');
         $.ajax({
@@ -222,16 +278,16 @@ $(document).ready(function () {
             dataType: "json",
             success: function(data)
             {
-                let currentPage = t1.page();
-                t1.row(rowIndex).remove().draw();
+                let currentPage = shop_table.page();
+                shop_table.row(rowIndex).remove().draw();
 
                 const modal = bootstrap.Modal.getInstance($("#delete_shop_modal"));
                 modal.hide();
 
-                $("#delete_modal_id").text('Delete');
+                $("#delete_shop").text('Delete');
 
                 // redrawing to original page
-                t1.page(currentPage).draw( 'page' );
+                shop_table.page(currentPage).draw( 'page' );
 
                 notyf.success({
                     message: 'Shop  Deleted <strong>Successfully !</strong>',
@@ -244,7 +300,7 @@ $(document).ready(function () {
             },
             error: function()
             {
-                $("#delete_modal_id").text('Delete');
+                $("#delete_shop").text('Delete');
                 notyf.error({
                     message: "<strong>Warning !</strong> Can't Delete shop",
                     duration: 7000,
@@ -256,20 +312,17 @@ $(document).ready(function () {
     /* ### Delete Data Start ### */
 
     //Reset Input when close modal
-    $('#Submit_shop_modal').on('hidden.bs.modal', function () {
+    $('#add_shop_modal').on('hidden.bs.modal', function () {
         $(this).find('#shop_form').trigger('reset');
     });
-    $('#update_shop_modal1').on('hidden.bs.modal', function () {
+    $('#update_shop_modal').on('hidden.bs.modal', function () {
         $(this).find('#update_shop_form').trigger('reset');
     });
 
 });
 
 
-
-// pdf, excel etc hide useless collumn, full page, proper collumn size
-
-// proper commenting and intending all codes
+// proper commenting and intending all codes-------------------------
 
 // after apu, handle server level errors
 
